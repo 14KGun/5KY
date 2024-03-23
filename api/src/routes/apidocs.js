974649c50ea4d5aae2a10ec2e40b6b6dec84f599
Geon Kim <g.kim@teamsparta.co>
2024-03-23 15:40:45 +0000

    Fix: apidocs

--- api/src/routes/apidocs.js
+++ api/src/routes/apidocs.js
@@ -14,7 +14,7 @@ router.use(
           title: "5KY API Docs",
           description: "날 참고해랏",
         },
-        servers: [{ url: "http://localhost:8080" }],
+        servers: [{ url: "http://ec2-13-124-193-99.ap-northeast-2.compute.amazonaws.com:8081" }, { url: "http://localhost:8080" }],
         basePath: "/",
       },
       apis: ["./src/routes/*.js"], //Swagger 파일 연동
