9808315b1742f0521a88a111f163864e32516900
GKim67 <g.kim@teamsparta.co>
2024-03-24 05:01:09 +0900

    Add: isBookmarked

--- api/src/routes/history.js
+++ api/src/routes/history.js
@@ -26,6 +26,8 @@ const router = express.Router();
  *                      type: array
  *                      items:
  *                        type: string
+ *                    isBookmarked:
+ *                      type: boolean
  *                    users:
  *                      type: array
  *                      items:
@@ -61,6 +63,7 @@ router.get(
       historys.map((history) => ({
         _id: history._id,
         joinAts: history.joinAts,
+        isBookmarked: history.isBookmarked,
         users: history.userIds.map((user) => ({
           _id: user._id,
           id: user.id,
@@ -79,32 +82,77 @@ router.get(
  *  /history/byId/{id}:
  *    get:
  *      summary: 히스토리 정보 조회
- *      tags: [history(X)]
+ *      tags: [history]
  *      parameters:
  *        - in: path
  *          name: id
  *          required: true
  *          schema:
  *            type: string
- *        - in: body
- *          name: category
- *          required: false
- *          schema:
- *            type: object
- *            required:
- *              - category
- *            properties:
- *              historyName:
- *                type: string
- *              firstName:
- *                type: string
  *      responses:
- *       200:
- *        description: 로그인 성공
+ *        200:
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: object
+ *                required: [_id, joinAts, users]
+ *                properties:
+ *                  _id:
+ *                    type: string
+ *                  joinAts:
+ *                    type: array
+ *                    items:
+ *                      type: string
+ *                  isBookmarked:
+ *                    type: boolean
+ *                  users:
+ *                    type: array
+ *                    items:
+ *                      type: object
+ *                      required: [_id, id, name, tags, age, gender]
+ *                      properties:
+ *                        _id:
+ *                          type: string
+ *                        id:
+ *                          type: string
+ *                        name:
+ *                          type: string
+ *                        tags:
+ *                          type: array
+ *                          items:
+ *                            type: string
+ *                        age:
+ *                          type: number
+ *                        gender:
+ *                          type: string
+ *                          enum: [male, female, unknown]
  */
-router.get("/byMe", (req, res) => {
-  console.log("POST /");
-  res.send("POST /");
-});
+router.get(
+  "/byId/:id",
+  authGuard,
+  wrapAsyncController(async (req, res) => {
+    const history = await historyModel
+      .findOne({
+        $and: [
+          { userIds: { $elemMatch: { $eq: req.user._id } } },
+          { _id: req.params.id },
+        ],
+      })
+      .populate("userIds");
+    res.json({
+      _id: history._id,
+      joinAts: history.joinAts,
+      isBookmarked: history.isBookmarked,
+      users: history.userIds.map((user) => ({
+        _id: user._id,
+        id: user.id,
+        name: user.name,
+        tags: user.tags,
+        age: user.age,
+        gender: user.gender,
+      })),
+    });
+  })
+);
 
 module.exports = router;
