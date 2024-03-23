3823f38573a77fa712e9d3e06dae132ad912faa7
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-24 00:35:57 +0900

    cors test

--- web/src/screens/Login/Login.jsx
+++ web/src/screens/Login/Login.jsx
@@ -1,6 +1,8 @@
 import "./Login.css";
 import styled from "styled-components";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { useCallback, useState } from "react";
+import instance from "../../utils/instance";
 
 const Container = styled.div`
   width: 100vw;
@@ -14,7 +16,7 @@ const Container = styled.div`
 const Title = styled.div`
   font-size: 50px;
   font-weight: Bold;
-  color: #F36427;
+  color: #f36427;
   margin-bottom: 71px;
 `;
 
@@ -39,7 +41,7 @@ const Input = styled.input`
   padding: 0 15px;
   &:focus {
     outline: none;
-    box-shadow: 0px 0px 2px #F36427;
+    box-shadow: 0px 0px 2px #f36427;
   }
 `;
 
@@ -50,7 +52,7 @@ const LoginBtn = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: #F36427;
+  background-color: #f36427;
   color: white;
   font-size: 16px;
   font-weight: 750;
@@ -72,7 +74,7 @@ const Info = styled.div`
 `;
 
 const SignupBtn = styled.div`
-  color: #F36427;
+  color: #f36427;
   font-weight: 700;
   margin-left: 5px;
   &:hover {
@@ -82,22 +84,43 @@ const SignupBtn = styled.div`
 
 const Login = () => {
   const navigate = useNavigate();
+  const [id, setId] = useState();
+  const [pw, setPw] = useState();
+
+  const onClickLogin = useCallback(async () => {
+    try {
+      const { data } = await instance.post("/auth", { id, password: pw });
+    } catch (error) {
+      console.log(error);
+    }
+  }, [id, pw]);
 
   const handleSignupClick = () => {
-    navigate('/signup');
+    navigate("/signup");
   };
+  
   return (
     <Container>
       <Title>WOOYEON?</Title>
       <IdContainer>
         <Label>아이디</Label>
-        <Input type="text" placeholder="아이디를 입력해주세요"></Input>
+        <Input
+          type="text"
+          placeholder="아이디를 입력해주세요"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+        ></Input>
       </IdContainer>
       <IdContainer>
         <Label>비밀번호</Label>
-        <Input type="password" placeholder="비밀번호를 입력해주세요"></Input>
+        <Input
+          type="password"
+          placeholder="비밀번호를 입력해주세요"
+          value={pw}
+          onChange={(e) => setPw(e.target.value)}
+        ></Input>
       </IdContainer>
-      <LoginBtn>로그인</LoginBtn>
+      <LoginBtn onClick={onClickLogin}>로그인</LoginBtn>
       <ToSignUp>
         <Info>아직 계정이 없으신가요?</Info>
         <SignupBtn onClick={handleSignupClick}>회원가입</SignupBtn>
