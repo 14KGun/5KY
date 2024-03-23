4ad81dea258f8cda8b7b1e16ed851d9b02879490
Woojeong Choi <gldeel@kaist.ac.kr>
2024-03-23 17:14:35 +0900

    add font and make history card

---
+++ web/src/components/HistoryCard/HistoryCard.jsx
@@ -0,0 +1,8 @@
+const HistoryCard = () => {
+    return (
+      <div>
+        <h2>히스토리 카드</h2>
+      </div>
+    );};
+  
+  export default HistoryCard;
--- web/src/screens/History/History.jsx
+++ web/src/screens/History/History.jsx
@@ -1,7 +1,12 @@
+import HistoryCard from "../../components/HistoryCard/HistoryCard";
+
 const History = () => {
     return (
-      <div>
-        <h1>히스토리</h1>
+      <div className="container">
+        <h1>오늘의 우연</h1>
+        <HistoryCard></HistoryCard>
+        <HistoryCard></HistoryCard>
+
       </div>
     );};
   
