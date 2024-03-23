71fa8004aa9e431368be595ea853714542dc594e
GKim67 <g.kim@teamsparta.co>
2024-03-24 03:25:20 +0900

    Add: findNearUserPairs

--- api/src/modules/models/user.model.js
+++ api/src/modules/models/user.model.js
@@ -1,6 +1,15 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
+const wishSchema = Schema(
+  {
+    minAge: { type: Number, required: true },
+    maxAge: { type: Number, required: true },
+    gender: { type: String, required: true, enum: ["male", "female"] },
+  },
+  { timestamps: true }
+);
+
 const userSchema = Schema(
   {
     id: { type: String, required: true, unique: true },
@@ -9,6 +18,8 @@ const userSchema = Schema(
     tags: { type: [String], required: true, default: [] },
     age: { type: Number, required: true },
     gender: { type: String, required: true, enum: ["male", "female"] },
+    friend: { type: wishSchema, required: false },
+    lover: { type: wishSchema, required: false },
   },
   { timestamps: true }
 );
